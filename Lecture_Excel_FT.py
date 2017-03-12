1457c131db6b1f181f7741c8f7ede58794b3e607
joelTo <joel.torib@gmail.com>
2017-03-12 17:53:27 +0100

    Importation des 2 fichiers a compare l'un avec l'autre

--- Lecture_Excel_FT.py
+++ Lecture_Excel_FT.py
@@ -6,6 +6,7 @@ wb_comparaison = xlrd.open_workbook('FT_MODE_20170307.xlsx')
 Feuille_reference = wb_reference.sheet_names()
 Feuille_comparaison = wb_comparaison.sheet_names()
 
+#--------------------INPUT Reference -------------------------
 
 sh=wb_reference.sheet_by_name(u'IBM Rational ClearQuest Web')
 #recupere la colonne Id du tableau de reference tout doit sortie de ClearQuest
@@ -20,7 +21,33 @@ coloneState=sh.col_values(2)
 #recupere la colonne  de la gravte de reference tout doit sortie de ClearQuest
 coloneGravite=sh.col_values(3)
 
-print coloneId[2]
 
-#for ligne in coloneId:
-#    print "coloneId :",ligne
+inputExcelReference={}
+for idx,ligne in enumerate(coloneId):
+    inputExcelReference[ligne]={coloneLibelle[idx],coloneState[idx],coloneGravite[idx]}
+
+#----------------------
+sh=wb_comparaison.sheet_by_name(u'IBM Rational ClearQuest Web')
+#recupere la colonne Id du tableau de reference tout doit sortie de ClearQuest
+coloneId_comparaison = sh.col_values(0)
+
+#recupere la colonne libelle du tableau de reference tout doit sortie de ClearQuest
+coloneLibelle_comparaison= sh.col_values(1)
+
+#recupere la colonne State du tableau de reference tout doit sortie de ClearQuest
+coloneState_comparaison=sh.col_values(2)
+
+#recupere la colonne  de la gravte de reference tout doit sortie de ClearQuest
+coloneGravite_comparaison=sh.col_values(3)
+
+
+inputExcelComparaison={}
+for idx,ligne in enumerate(coloneId_comparaison):
+    inputExcelComparaison[ligne]={coloneLibelle_comparaison[idx],coloneState_comparaison[idx],coloneGravite_comparaison[idx]}
+ #---------------------  
+
+print inputExcelReference['CNAM_00324999']
+if inputExcelComparaison['CNAM_00324999']:
+    print inputExcelComparaison['CNAM_00324999']
+
+ print 'je suis passe'  
