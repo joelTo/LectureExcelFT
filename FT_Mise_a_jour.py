356b0b0cd304b63da727391d59a06b250fe54404
joelTo <joel.torib@gmail.com>
2017-03-17 06:06:20 +0100

    avancement changement de statégie: On met tout sur notre fichier FT deja existant que l'on met a jour

--- FT_Mise_a_jour.py
+++ FT_Mise_a_jour.py
@@ -1,8 +1,11 @@
 import xlrd
 import xlwt
 
-wb_reference = xlrd.open_workbook('QueryResult.xlsx')
-wb_comparaison = xlrd.open_workbook('FT_MODE_20170307.xlsx')
+fichier_reference='QueryResult.xlsx'
+fichier_a_comparer='FT_MODE_20170307.xlsx'
+
+wb_reference = xlrd.open_workbook(fichier_reference)
+wb_comparaison = xlrd.open_workbook(fichier_a_comparer)
 
 Feuille_reference = wb_reference.sheet_names()
 Feuille_comparaison = wb_comparaison.sheet_names()
@@ -11,61 +14,73 @@ Feuille_comparaison = wb_comparaison.sheet_names()
 
 sh=wb_reference.sheet_by_name(u'IBM Rational ClearQuest Web')
 #recupere la colonne Id du tableau de reference tout doit sortie de ClearQuest
-coloneId_reference = sh.col_values(0)
+colonneId_reference = sh.col_values(0)
 
 #recupere la colonne libelle du tableau de reference tout doit sortie de ClearQuest
-coloneLibelle_reference= sh.col_values(1)
+colonneLibelle_reference= sh.col_values(1)
 
 #recupere la colonne State du tableau de reference tout doit sortie de ClearQuest
-coloneState_reference=sh.col_values(2)
+colonneState_reference=sh.col_values(2)
 
 #recupere la colonne  de la gravte de reference tout doit sortie de ClearQuest
-coloneGravite_reference=sh.col_values(3)
+colonneGravite_reference=sh.col_values(3)
+
+#recupere la date de creation du document de reference 
+colonneDateCreation_reference=sh.col_values(4)
+
+#recupere la DateDerniereModif du document de Reference
+colonneDateModif_reference=sh.col_values(5)
+
+#recupere le code de la FT du document de Reference
+colonneACP_reference=sh.col_values(6)
 
 
 
 inputExcelReference={}
 inputExcelReferenceParCategorie={}
-for idx,ligne in enumerate(coloneId_reference):
-    inputExcelReference[ligne]={coloneLibelle_reference[idx],coloneState_reference[idx],coloneGravite_reference[idx]}
-    inputExcelReferenceParCategorie[ligne]={'libelle':coloneLibelle_reference[idx],'state':coloneState_reference[idx],'gravite':coloneGravite_reference[idx]}
+for idx,ligne in enumerate(colonneId_reference):
+    inputExcelReference[ligne]={colonneLibelle_reference[idx],colonneState_reference[idx],colonneGravite_reference[idx],colonneDateCreation_reference[idx],colonneDateModif_reference[idx],colonneACP_reference[idx]}
+    inputExcelReferenceParCategorie[ligne]={'libelle':colonneLibelle_reference[idx],'state':colonneState_reference[idx],'gravite':colonneGravite_reference[idx],'dateCreation':colonneDateCreation_reference[idx],'dateModif':colonneDateModif_reference[idx],'acp':colonneACP_reference[idx]}
 
 #----------------------
 sh=wb_comparaison.sheet_by_name(u'IBM Rational ClearQuest Web')
 #recupere la colonne Id du tableau de reference tout doit sortie de ClearQuest
-coloneId_comparaison = sh.col_values(1)
+colonneId_comparaison = sh.col_values(1)
 
 #recupere la colonne libelle du tableau de reference tout doit sortie de ClearQuest
-coloneLibelle_comparaison= sh.col_values(2)
+colonneLibelle_comparaison= sh.col_values(2)
 
 #recupere la colonne State du tableau de reference tout doit sortie de ClearQuest
-coloneState_comparaison=sh.col_values(3)
+colonneState_comparaison=sh.col_values(3)
 
 #recupere la colonne  de la gravte de reference tout doit sortie de ClearQuest
-coloneGravite_comparaison=sh.col_values(4)
+colonneGravite_comparaison=sh.col_values(4)
 
 
 
 
 inputExcelComparaison={}
 inputExcelComparaisonParCategorie={}
-for idx,ligne in enumerate(coloneId_comparaison):
-    inputExcelComparaison[ligne]={coloneLibelle_comparaison[idx],coloneState_comparaison[idx],coloneGravite_comparaison[idx]}
-    inputExcelComparaisonParCategorie[ligne]={'libelle':coloneLibelle_comparaison[idx],'state':coloneState_comparaison[idx],'gravite':coloneGravite_comparaison[idx]}
-    
+for idx,ligne in enumerate(colonneId_comparaison):
+    inputExcelComparaison[ligne]={colonneLibelle_comparaison[idx],colonneState_comparaison[idx],colonneGravite_comparaison[idx]}
+    inputExcelComparaisonParCategorie[ligne]={'libelle':colonneLibelle_comparaison[idx],'state':colonneState_comparaison[idx],'gravite':colonneGravite_comparaison[idx]}
+   
  #---------------------  
 
 #Affiche les FT manquantes
-r1,r2= set(coloneId_reference),set(coloneId_comparaison)
+r1,r2= set(colonneId_reference),set(colonneId_comparaison)
 print "---------------------------------------------------------------"
 print "---------------------------------------------------------------"
 print "-   ","Nbr de FT manquante dans le document est de : ", len(r1.difference(r2))
-print "-   ",r1.difference(r2)
+ListeFtManquante=r1.difference(r2)
+print "-   ",ListeFtManquante
 print "---------------------------------------------------------------"
 print "---------------------------------------------------------------"
 
-
-
+#Affichage + rajout dans le futur fichier l'ecrite à la dernière place de cette FT
+for item in ListeFtManquante:
+    print item,inputExcelReference[item]
+    
 
 listeFtReference= inputExcelReference.keys()
 nbrModif=0
